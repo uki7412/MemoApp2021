1e05f746e272ff90b9fb1e2581ff2ee2697a3c62
Yuki Matsumoto <la.ra.ra.7412@gmail.com>
2021-01-29 05:40:07 +0800

    fetch data from firestore on MemoDetailScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -8,6 +8,8 @@ import {
   shape, string, instanceOf, arrayOf,
 } from 'prop-types';
 
+import { dateToString } from '../utils';
+
 export default function MemoList(props) {
   const { memos } = props;
   const navigation = useNavigation();
@@ -16,11 +18,11 @@ export default function MemoList(props) {
     return (
       <TouchableOpacity
         style={styles.memoListItems}
-        onPress={() => { navigation.navigate('MemoDetail'); }}
+        onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
       >
         <View>
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+          <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { shape, string } from 'prop-types';
 import {
   View, ScrollView, Text, StyleSheet,
 } from 'react-native';
+
+import firebase from 'firebase';
+
 import CircleButton from '../components/CircleButton';
+import { dateToString } from '../utils';
 
 export default function MemoDetailScreen(props) {
-  const { navigation } = props;
+  const { navigation, route } = props;
+  const { id } = route.params;
+  console.log(id);
+  const [memo, setMemo] = useState(null);
+
+  useEffect(() => {
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      unsubscribe = ref.onSnapshot((doc) => {
+        console.log(doc.id, doc.data());
+        const data = doc.data();
+        setMemo({
+          id: doc.id,
+          bodyText: data.bodyText,
+          updatedAt: data.updatedAt.toDate(),
+        });
+      });
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <View sytyle={styles.container}>
       <View style={styles.memoHeader}>
-        <Text style={styles.memoTitle}>買い物リスト</Text>
-        <Text style={styles.memoDate}>2021/01/12</Text>
+        <Text style={styles.memoTitle} numberOfLines={1}>{memo && memo.bodyText}</Text>
+        <Text style={styles.memoDate}>{memo && dateToString(memo.updatedAt)}</Text>
       </View>
       <ScrollView style={styles.memoBody}>
         <Text style={styles.memoText}>
-          xxxxxxxxxxxxxxxxxxxxxxxxx
+          {memo && memo.bodyText}
         </Text>
       </ScrollView>
       <CircleButton
@@ -25,6 +53,12 @@ export default function MemoDetailScreen(props) {
     </View>
   );
 }
+
+MemoDetailScreen.propTypes = {
+  route: shape({
+    params: shape({ id: string }),
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
