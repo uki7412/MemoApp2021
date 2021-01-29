57a653cecf30d340226877f600ade625bb9a9cfe
Yuki Matsumoto <la.ra.ra.7412@gmail.com>
2021-01-30 01:09:12 +0800

    update memo data on MemoEditScreen merge: true

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,61 +1,62 @@
 import React, { useState } from 'react';
 import {
-  View, TextInput, StyleSheet,
+  View, TextInput, StyleSheet, Alert,
 } from 'react-native';
 import { shape, string } from 'prop-types';
-// import firebase from 'firebase';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 // import { translateErrors } from '../utils';
 
 export default function MemoEditScreen(props) {
-  const { navigation } = props;
-  // const { bodyText } = route.params;
-  // const [body, setBody] = useState(bodyText);
+  const { navigation, route } = props;
+  const { id, bodyText } = route.params;
+  const [body, setBody] = useState(bodyText);
 
-  // function handlePress() {
-  //   const { currentUser } = firebase.auth();
-  //   if (currentUser) {
-  //     const db = firebase.firestore();
-  //     const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
-  //     ref.set({
-  //       bodyText: body,
-  //       updatedAt: new Date(),
-  //     }, { merge: true })
-  //       .then(() => {
-  //         navigation.goBack();
-  //       })
-  //       .catch((error) => {
-  //         const errorMsg = translateErrors(error.code);
-  //         Alert.alert(errorMsg.title, errorMsg.description);
-  //       });
-  //   }
-  // }
+  function handlePress() {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      ref.set({
+        bodyText: body,
+        updatedAt: new Date(),
+      }, { merge: true })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch((error) => {
+          // const errorMsg = translateErrors(error.code);
+          // Alert.alert(errorMsg.title, errorMsg.description);
+          Alert.alert(error.code);
+        });
+    }
+  }
 
   return (
     <KeyboardSafeView style={styles.container} behavior="height">
       <View style={styles.inputContainer}>
         <TextInput
-          value="ゆむゆむooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+          value={body}
           multiline
           style={styles.input}
-            // onChangeText={(text) => { setBody(text); }}
+          onChangeText={(text) => { setBody(text); }}
         />
       </View>
       <CircleButton
         name="check"
-        onPress={() => {navigation.goBack();}}
+        onPress={handlePress}
       />
     </KeyboardSafeView>
   );
 }
 
-// MemoEditScreen.propTypes = {
-//   route: shape({
-//     params: shape({ id: string, bodyText: string }),
-//   }).isRequired,
-// };
+MemoEditScreen.propTypes = {
+  route: shape({
+    params: shape({ id: string, bodyText: string }),
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
